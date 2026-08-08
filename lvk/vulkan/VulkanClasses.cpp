82542381a9a9896af649bcf1618cda38b0636d23
rokuz <5437220+rokuz@users.noreply.github.com>
2026-08-08 11:47:45 +0100

    Track async-compute submits in deferred tasks
    A deferred resource can be in flight on either queue and the two submit
    timelines are independent, so `DeferredTask` now also stores a submit handle
    for the async-compute queue. `processDeferredTasks()` and `waitDeferredTasks()`
    wait on both timelines before running the task.

--- lvk/vulkan/VulkanClasses.cpp
+++ lvk/vulkan/VulkanClasses.cpp
@@ -799,9 +799,11 @@ VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormat2KHR
 namespace lvk {
 
 struct DeferredTask {
-  DeferredTask(std::packaged_task<void()>&& task, SubmitHandle handle) : task_(std::move(task)), handle_(handle) {}
+  DeferredTask(std::packaged_task<void()>&& task, SubmitHandle handle, SubmitHandle handleCompute) :
+    task_(std::move(task)), handle_(handle), handleCompute_(handleCompute) {}
   std::packaged_task<void()> task_;
   SubmitHandle handle_;
+  SubmitHandle handleCompute_;
 };
 
 struct VulkanContextImpl final {
@@ -9027,7 +9029,9 @@ void lvk::VulkanContext::deferredTask(std::packaged_task<void()>&& task, SubmitH
   if (handle.empty()) {
     handle = immediate_->getNextSubmitHandle();
   }
-  pimpl_->deferredTasks_.emplace_back(std::move(task), handle);
+  // A resource can be in flight on either queue, and the two submit timelines are independent.
+  const SubmitHandle handleCompute = immediateCompute_ ? immediateCompute_->getNextSubmitHandle() : SubmitHandle();
+  pimpl_->deferredTasks_.emplace_back(std::move(task), handle, handleCompute);
 }
 
 void* lvk::VulkanContext::getVmaAllocator() const {
@@ -9037,7 +9041,8 @@ void* lvk::VulkanContext::getVmaAllocator() const {
 void lvk::VulkanContext::processDeferredTasks() const {
   std::vector<DeferredTask>::iterator it = pimpl_->deferredTasks_.begin();
 
-  while (it != pimpl_->deferredTasks_.end() && immediate_->isReady(it->handle_, true)) {
+  while (it != pimpl_->deferredTasks_.end() && immediate_->isReady(it->handle_, true) &&
+         (!immediateCompute_ || immediateCompute_->isReady(it->handleCompute_, true))) {
     (it++)->task_();
   }
 
@@ -9047,6 +9052,9 @@ void lvk::VulkanContext::processDeferredTasks() const {
 void lvk::VulkanContext::waitDeferredTasks() {
   for (auto& task : pimpl_->deferredTasks_) {
     immediate_->wait(task.handle_);
+    if (immediateCompute_ && !task.handleCompute_.empty()) {
+      immediateCompute_->wait(task.handleCompute_);
+    }
     task.task_();
   }
   pimpl_->deferredTasks_.clear();
